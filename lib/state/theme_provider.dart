d656efaeed42af6d418d6ba8eef5ede85c8ef8d6
Cristian Paulo <cristianpaulo4@gmail.com>
2026-07-24 16:08:01 -0300

    chore: update dependency packages in node_modules

--- lib/state/theme_provider.dart
+++ lib/state/theme_provider.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String _themeKey = 'theme_mode';
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.dark);
+  ThemeNotifier() : super(ThemeMode.dark) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt(_themeKey) ?? 1; // Default: dark (1)
+    state = ThemeMode.values[themeIndex];
+  }
+
+  Future<void> _saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themeKey, state.index);
+  }
 
   void toggleTheme() {
     state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    _saveTheme();
   }
 
   void setThemeMode(ThemeMode mode) {
     state = mode;
+    _saveTheme();
   }
 }
 
